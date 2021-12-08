7d87c4ea8844582b948eec7084f2efb3cda642a0
vicchu <Chjun-Chi.Chiu@vitecsoftware.com>
2021-12-08 18:54:16 +0100

    Day 8

---
+++ src/main/java/classes/InputOutput.java
@@ -0,0 +1,13 @@
+package classes;
+
+import java.util.ArrayList;
+
+public class InputOutput {
+    public ArrayList<String> in;
+    public ArrayList<String> out;
+
+    public InputOutput() {
+        this.in = new ArrayList<>();
+        this.out = new ArrayList<>();
+    }
+}
