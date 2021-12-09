853115a88078198f1faa362a148ac1a4799a07a4
vicchu <Chjun-Chi.Chiu@vitecsoftware.com>
2021-12-09 14:07:48 +0100

    Day 9

--- src/main/java/util/FileUtil.java
+++ src/main/java/util/FileUtil.java
@@ -31,4 +31,13 @@ public final class FileUtil {
         }
         return nr;
     }
+
+    private void print(int[][] matrix) {
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                System.out.print(anInt);
+            }
+            System.out.println();
+        }
+    }
 }
--- src/test/java/days/Day9Test.java
+++ src/test/java/days/Day9Test.java
@@ -1,5 +1,7 @@
 package days;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
 import util.FileUtil;
@@ -8,7 +10,25 @@ class Day9Test {
 
     @Test
     void testPart1ex() throws IOException {
-        new Day9().part1(FileUtil.readFromFile("day9ex.txt"));
+        int res = new Day9().part1(FileUtil.readFromFile("day9ex.txt"));
+        assertEquals(15, res);
+    }
 
+    @Test
+    void testPart1() throws IOException {
+        int res = new Day9().part1(FileUtil.readFromFile("day9.txt"));
+        assertEquals(572, res);
+    }
+
+    @Test
+    void testPart2ex() throws IOException {
+        long res = new Day9().part2(FileUtil.readFromFile("day9ex.txt"));
+        assertEquals(1134, res);
+    }
+
+    @Test
+    void testPart2() throws IOException {
+        long res = new Day9().part2(FileUtil.readFromFile("day9.txt"));
+        assertEquals(847044, res);
     }
 }
