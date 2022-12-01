0a694aecbeeabbaf05209fc9f770ce3b5027f035
Xarczuz <chjunchi.chiu@gmail.com>
2022-12-01 17:50:28 +0100

    day1

--- src/main/java/adventOfCode/Year2022/Day1.java
+++ src/main/java/adventOfCode/Year2022/Day1.java
@@ -4,13 +4,34 @@ import util.FileUtil;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Day1 {
 
-
     public static void main(String[] args) throws IOException, URISyntaxException {
         List<String> l = FileUtil.readfile(Day1.class, 1);
+        oneStar(l);
+        twoStar(l);
+    }
+
+    private static void twoStar(List<String> l) {
+        LinkedList<Long> calories = new LinkedList<>();
+        long nr = 0;
+        for (String s : l) {
+            if (s.isBlank()) {
+                calories.add(nr);
+                nr = 0;
+            } else {
+                nr += Long.parseLong(s);
+            }
+        }
+        calories.sort(Long::compareTo);
+
+        System.out.println("Tot: " + (calories.removeLast() + calories.removeLast() + calories.removeLast()));
+    }
+
+    private static void oneStar(List<String> l) {
         long max = 0;
         long nr = 0;
         for (String s : l) {
@@ -21,6 +42,6 @@ public class Day1 {
                 max = Math.max(nr, max);
             }
         }
-        System.out.println(max);
+        System.out.println("Most calories: " + max);
     }
 }
