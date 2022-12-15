16c3c0ca67bc95ed83456a7b27af51dd5884b436
Xarczuz <chjunchi.chiu@gmail.com>
2022-12-15 19:31:42 +0100

    day10

--- src/main/java/adventOfCode/Year2022/Day10.java
+++ src/main/java/adventOfCode/Year2022/Day10.java
@@ -17,16 +17,18 @@ public class Day10 {
         List<String> l2 = FileUtil.readfileExempel(Day10.class);
         List<String> l3 = FileUtil.readfileExempel2(Day10.class);
         TimeUtil.startTime();
-        oneStar(l);
-        oneStar(l2);
-        oneStar(l3);
+//        oneStar(l);
+//        oneStar(l2);
+//        oneStar(l3);
         TimeUtil.endTime();
         TimeUtil.startTime();
         twoStar(l2);
-        //twoStar(l);
+        twoStar(l);
         TimeUtil.endTime();
     }
 
+    //RAPFKIHF
+    //
     private static void oneStar(List<String> l) {
         int[] ints = {20, 60, 100, 140, 180, 220};
         int sum = 0;
@@ -69,20 +71,105 @@ public class Day10 {
                 instructions.add(instruction1);
             }
 
-
-//            System.out.println("cycle: " + cycle + " x: " + x);
             for (int anInt : ints) {
                 if (cycle == anInt) {
                     sum += anInt * x;
                 }
             }
-
         }
         System.out.println("One star: " + sum);
 
     }
 
     private static void twoStar(List<String> l) {
+        int[] ints = {20, 60, 100, 140, 180, 220};
+        int sum = 0;
+        LinkedList<String> list = new LinkedList<>(l);
+        int x = 1;
+        int spirite = 1;
+        int row = 0;
+        ArrayList<String[]> screen = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            String[] screen1 = new String[40];
+            for (int j = 0; j < screen1.length; j++) {
+                screen1[j] = ".";
+            }
+            screen.add(screen1);
+
+        }
+        int count = 0;
+        ArrayList<Instruction> instructions = new ArrayList<>();
+        for (int cycle = 0; cycle < 240; cycle++) {
+            if (count == 40) {
+                for (String[] strings : screen) {
+                    for (int i = 0; i < strings.length; i++) {
+                        String string = strings[i];
+                        System.out.print(string);
+                        strings[i] = ".";
+                    }
+                    break;
+                }
+                System.out.println();
+                count = 0;
+            }
+            count++;
+            String instruction = "";
+            if (!list.isEmpty() && instructions.isEmpty()) {
+                instruction = list.removeFirst();
+            }
+            if (instruction.contains("noop")) {
+                Instruction e = new Instruction();
+                e.cycleToAdd = cycle;
+                instructions.add(e);
+
+            } else if (instruction.contains("addx")) {
+                String stringNr = instruction.split(" ")[1];
+                int intNr = Integer.parseInt(stringNr);
+                Instruction instruction1 = new Instruction();
+                instruction1.cycleToAdd = cycle + 1;
+                instruction1.value = intNr;
+                instructions.add(instruction1);
+            }
+
+            if (spirite == cycle % 40 + 1) {
+                screen.get(row)[cycle % 40] = "#";
+            } else if (spirite == cycle % 40 - 1) {
+                screen.get(row)[cycle % 40] = "#";
+            } else if (spirite == cycle % 40) {
+                screen.get(row)[cycle % 40] = "#";
+            } else {
+                screen.get(row)[cycle % 40] = ".";
+            }
+            ArrayList<UUID> uuidsToDelete = new ArrayList<>();
+            for (Instruction inst : instructions) {
+                if (inst.cycleToAdd == cycle) {
+                    x += inst.value;
+                    uuidsToDelete.add(inst.uuid);
+                }
+            }
+            for (UUID uuid : uuidsToDelete) {
+                int i = 0;
+                for (; i < instructions.size(); i++) {
+                    Instruction inst = instructions.get(i);
+                    if (inst.uuid == uuid) {
+                        break;
+                    }
+                }
+                instructions.remove(i);
+            }
+
+            spirite = x;
+        }
+        for (String[] strings : screen) {
+            for (String string : strings) {
+                System.out.print(string);
+            }
+            System.out.println();
+            break;
+        }
+
+        System.out.println();
+        System.out.println("Two star: " + sum);
 
     }
 
