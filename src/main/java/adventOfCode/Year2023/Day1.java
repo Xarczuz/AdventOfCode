595520ffb95cbfb5ec7f552d60d892f821d622dd
chjun <chjunchi.chiu@gmail.com>
2023-11-30 17:17:13 +0100

    Day 1 preparation

---
+++ src/main/java/adventOfCode/Year2023/Day1.java
@@ -0,0 +1,25 @@
+package adventOfCode.Year2023;
+
+import util.FileUtil;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Day1 {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        List<String> l = FileUtil.readfile(Day1.class);
+        oneStar(l);
+        twoStar(l);
+    }
+
+    private static void twoStar(List<String> l) {
+
+    }
+
+    private static void oneStar(List<String> l) {
+
+    }
+}
