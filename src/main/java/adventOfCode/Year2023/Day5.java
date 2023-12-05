a8753ec705858966805050e2cf0130c9b3d36f89
chjun <chjunchi.chiu@gmail.com>
2023-12-05 17:41:24 +0100

    2023 Day 5 part 2 DONE

--- src/main/java/adventOfCode/Year2023/Day5.java
+++ src/main/java/adventOfCode/Year2023/Day5.java
@@ -16,38 +16,65 @@ public class Day5 {
         List<String> l = FileUtil.readfile(Day5.class);
         List<String> l2 = FileUtil.readfileExempel(Day5.class);
         TimeUtil.startTime();
-//        oneStar(l); // 910845529
-//        oneStar(l2);
+        oneStar(l); // 910845529
+        oneStar(l2);
         TimeUtil.endTime();
         TimeUtil.startTime();
-//        twoStar(l);
+        twoStar(l); // 77435348
         twoStar(l2);
         TimeUtil.endTime();
     }
 
     private static void twoStar(List<String> l) {
         Farm farm = parseStrings(l);
-        initSeeds(farm);
         long location = Long.MAX_VALUE;
-        for (Long seed : farm.seeds) {
-            for (SeedMap seedMap : farm.seedMaps) {
-                seed = convertSeed(seed, seedMap);
+        invertMap(farm);
+        for (long i = 0; i < Long.MAX_VALUE; i++) {
+            location = i;
+            long temp = i;
+            ArrayList<SeedMap> seedMaps = farm.seedMaps;
+            for (int j = seedMaps.size() - 1; j >= 0; j--) {
+                SeedMap seedMap = seedMaps.get(j);
+                temp = convertSeed2(temp, seedMap);
             }
-            location = Math.min(seed, location);
+            if (isConvertedToValidSeed(temp, farm)) {
+                break;
+            } else {
+                location = Long.MAX_VALUE;
+            }
+            if (i % 100000 == 0) {
+                System.out.println(i);
+            }
         }
         System.out.println("Two star: " + location);
     }
 
-    private static void initSeeds(Farm farm) {
-        ArrayList<Long> newSeeds = new ArrayList<>();
+    private static boolean isConvertedToValidSeed(long location, Farm farm) {
         ArrayList<Long> seeds = farm.seeds;
-
         for (int i = 0; i < seeds.size(); i += 2) {
-
-
+            Long seed = seeds.get(i);
+            Long range = seeds.get(i + 1);
+            if (location >= seed && location < seed + range) {
+                return true;
+            }
         }
-        System.out.println();
+        return false;
     }
+
+    private static void invertMap(Farm farm) {
+        for (SeedMap seedMap : farm.seedMaps) {
+            for (SeedMap.TheMap theMap : seedMap.theMaps) {
+                long d = theMap.destination;
+                long s = theMap.source;
+                theMap.source = d;
+                theMap.destination = s;
+            }
+        }
+        for (SeedMap seedMap : farm.seedMaps) {
+            seedMap.theMaps.sort(Comparator.comparingLong(o -> o.source));
+        }
+    }
+
 
     private static void oneStar(List<String> l) {
         Farm farm = parseStrings(l);
@@ -61,11 +88,24 @@ public class Day5 {
         System.out.println("One star: " + location);
     }
 
+    private static Long convertSeed2(Long seed, SeedMap seedMap) {
+        ArrayList<SeedMap.TheMap> theMaps = seedMap.theMaps;
+        for (SeedMap.TheMap map : theMaps) {
+            if (seed >= map.source && seed < map.source + map.range) {
+                if (map.source > map.destination) {
+                    seed = seed - Math.abs(map.source - map.destination);
+                } else if (map.source < map.destination) {
+                    seed = seed + Math.abs(map.source - map.destination);
+                }
+                break;
+            }
+        }
+        return seed;
+    }
+
     private static Long convertSeed(Long seed, SeedMap seedMap) {
         ArrayList<SeedMap.TheMap> theMaps = seedMap.theMaps;
-        theMaps.sort(Comparator.comparingLong(o -> o.source));
-        for (int i = 0; i < theMaps.size(); i++) {
-            SeedMap.TheMap map = theMaps.get(i);
+        for (SeedMap.TheMap map : theMaps) {
             if (seed >= map.source && seed <= map.source + map.range) {
                 if (map.source > map.destination) {
                     seed = seed - Math.abs(map.source - map.destination);
@@ -101,6 +141,7 @@ public class Day5 {
                 e.destination = Long.parseLong(numbers[0]);
                 e.source = Long.parseLong(numbers[1]);
                 e.range = Long.parseLong(numbers[2]);
+                seedMap.theMaps.sort(Comparator.comparingLong(o -> o.source));
             }
         }
         return new Farm(seeds, seedMaps);
