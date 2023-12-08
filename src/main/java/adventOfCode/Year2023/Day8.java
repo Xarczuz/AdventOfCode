a95ca7d7f172bf98b120e3eb4244ab2a61edf8cf
chjun <chjunchi.chiu@gmail.com>
2023-12-08 17:03:49 +0100

    2023 Day 8 part 2 TODO

--- src/main/java/adventOfCode/Year2023/Day8.java
+++ src/main/java/adventOfCode/Year2023/Day8.java
@@ -5,9 +5,7 @@ import util.TimeUtil;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class Day8 {
 
@@ -15,18 +13,81 @@ public class Day8 {
     public static void main(String[] args) throws IOException, URISyntaxException {
         List<String> l = FileUtil.readfile(Day8.class);
         List<String> l2 = FileUtil.readfileExempel(Day8.class);
+        List<String> l3 = FileUtil.readfileExempel2(Day8.class);
         TimeUtil.startTime();
-        oneStar(l);
-        oneStar(l2);
+//        oneStar(l);
+//        oneStar(l2);
         TimeUtil.endTime();
         TimeUtil.startTime();
-//        twoStar(l);
-//        twoStar(l2);
+//        twoStar(l3);
+        twoStar(l);
         TimeUtil.endTime();
     }
 
     private static void twoStar(List<String> l) {
+        Nodes node = parseString(l);
 
+        Path[] paths = new Path[999999];
+        convertToIntMap(node, paths);
+
+        ArrayList<String> startLocations = new ArrayList<>();
+        for (String s : node.paths.keySet()) {
+            if (s.charAt(2) == 'A') {
+                startLocations.add(s);
+            }
+        }
+        int[] locs = new int[startLocations.size()];
+        for (int i = 0; i < startLocations.size(); i++) {
+            String startLocation = startLocations.get(i);
+            locs[i] = convertToInt(startLocation);
+        }
+        long steps = 0;
+        do {
+            char direction = node.getDirection();
+            for (int i = 0; i < locs.length; i++) {
+                int startLocation = locs[i];
+                if (direction == 'R') {
+                    locs[i] = paths[startLocation].rightInt;
+                } else {
+                    locs[i] = paths[startLocation].leftInt;
+                }
+            }
+            steps++;
+            if (steps % 1000000 == 0) {
+                System.out.println(steps);
+            }
+        } while (!allAtZ(locs));
+        System.out.println("Two Star: " + steps);
+    }
+
+    private static void convertToIntMap(Nodes node, Path[] path) {
+        for (Map.Entry<String, Path> stringPathEntry : node.paths.entrySet()) {
+            int i = convertToInt(stringPathEntry.getKey());
+            Path value = stringPathEntry.getValue();
+            value.leftInt = convertToInt(value.left);
+            value.rightInt = convertToInt(value.right);
+
+            path[i] = value;
+        }
+    }
+
+    private static int convertToInt(String abc) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : abc.toCharArray()) {
+            stringBuilder.append((int) c);
+        }
+        return Integer.parseInt(stringBuilder.toString());
+    }
+
+    private static boolean allAtZ(int[] startLocations) {
+        for (int i = 1; i < startLocations.length; i++) {
+            int startLocation = startLocations[i];
+            int ss = startLocation % 100;
+            if (ss != 'Z') {
+                return false;
+            }
+        }
+        return true;
     }
 
     private static void oneStar(List<String> l) {
@@ -84,6 +145,8 @@ public class Day8 {
         String location;
         String left;
         String right;
+        int leftInt;
+        int rightInt;
 
         @Override
         public boolean equals(Object o) {
