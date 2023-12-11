bc3b678468897dbae98f115f6499142fdef4044f
chjun <chjunchi.chiu@gmail.com>
2023-12-11 18:50:05 +0100

    2023 Day 11 part 1 TODO

--- src/main/java/adventOfCode/Year2023/Day11.java
+++ src/main/java/adventOfCode/Year2023/Day11.java
@@ -5,6 +5,7 @@ import util.TimeUtil;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day11 {
@@ -14,12 +15,12 @@ public class Day11 {
         List<String> l = FileUtil.readfile(Day11.class);
         List<String> l2 = FileUtil.readfileExempel(Day11.class);
         TimeUtil.startTime();
-        oneStar(l);
+//        oneStar(l);
         oneStar(l2);
         TimeUtil.endTime();
         TimeUtil.startTime();
-        twoStar(l);
-        twoStar(l2);
+//        twoStar(l);
+//        twoStar(l2);
         TimeUtil.endTime();
     }
 
@@ -28,7 +29,31 @@ public class Day11 {
     }
 
     private static void oneStar(List<String> l) {
+        ArrayList<ArrayList<Character>> matrix = parseString(l);
 
+
+        for (int y = 0; y < matrix.size(); y++) {
+            for (int x = 0; x < matrix.getFirst().size(); x++) {
+
+
+            }
+        }
+        for (ArrayList<Character> characters : matrix) {
+            System.out.println(characters);
+        }
+
+    }
+
+    private static ArrayList<ArrayList<Character>> parseString(List<String> l) {
+        ArrayList<ArrayList<Character>> matrix = new ArrayList<>();
+        for (String s : l) {
+            ArrayList<Character> c = new ArrayList<>();
+            matrix.add(c);
+            for (char c1 : s.toCharArray()) {
+                c.add(c1);
+            }
+        }
+        return matrix;
     }
 
 
