c021da4e8e1131f19b2543cda119cd88f3320afd
chjun <chjunchi.chiu@gmail.com>
2024-08-29 13:06:58 +0200

    2023 Day 13 part 1

--- src/main/java/adventOfCode/Year2023/Day13.java
+++ src/main/java/adventOfCode/Year2023/Day13.java
@@ -5,6 +5,7 @@ import util.TimeUtil;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day13 {
@@ -17,10 +18,10 @@ public class Day13 {
         oneStar(l);
         oneStar(l2);
         TimeUtil.endTime();
-        TimeUtil.startTime();
-        twoStar(l);
-        twoStar(l2);
-        TimeUtil.endTime();
+//        TimeUtil.startTime();
+//        twoStar(l);
+//        twoStar(l2);
+//        TimeUtil.endTime();
     }
 
     private static void twoStar(List<String> l) {
@@ -28,7 +29,83 @@ public class Day13 {
     }
 
     private static void oneStar(List<String> l) {
+        long sum = 0;
+        ArrayList<String> arr = new ArrayList<>();
+        for (String s : l) {
+            if (!s.isEmpty()) {
+                arr.add(s);
+            } else {
+                sum += mirror(arr);
+                arr.clear();
+            }
+        }
+        sum += mirror(arr);
+        System.out.println(sum);
+    }
 
+    private static long mirror(ArrayList<String> arr) {
+        int sum = horizontalReflection(arr, false) * 100;
+        ArrayList<String> transposedArr = transposeArray(arr);
+        sum += verticalReflection(transposedArr);
+
+        return sum;
+    }
+
+    protected static ArrayList<String> transposeArray(ArrayList<String> arr) {
+        ArrayList<String> arrStrings = new ArrayList<>();
+        for (String s : arr) {
+            char[] charArray = s.toCharArray();
+            for (int i = charArray.length - 1; i >= 0; i--) {
+                char c = charArray[i];
+                if (arrStrings.size() < charArray.length) {
+                    arrStrings.add(String.valueOf(c));
+                } else {
+                    int abs = Math.abs(charArray.length - i) - 1;
+                    arrStrings.set(abs, arrStrings.get(abs) + c);
+                }
+            }
+        }
+
+        return arrStrings;
+    }
+
+    protected static int verticalReflection(ArrayList<String> arr) {
+        return horizontalReflection(arr, true);
+    }
+
+    protected static int horizontalReflection(ArrayList<String> arr, boolean fromVertical) {
+        String prev = arr.getFirst();
+        int refPoint = -1;
+        for (int i = 1; i < arr.size(); i++) {
+            String current = arr.get(i);
+            if (prev.equals(current)) {
+                if (compare2List(arr.subList(0, i), arr.subList(i, arr.size()))) {
+                    refPoint = i;
+                    break;
+                }
+            }
+            prev = current;
+        }
+        if (refPoint != -1) {
+            if (fromVertical) {
+                return arr.size() - refPoint;
+            }
+            return refPoint;
+        } else {
+            return 0;
+        }
+    }
+
+    private static boolean compare2List(List<String> firstPart, List<String> secondPart) {
+        int index = Math.min(firstPart.size(), secondPart.size()) - 1;
+        for (int i = 0, j = firstPart.size() - 1; 0 <= index; i++, j--, index--) {
+            String s1 = firstPart.get(j);
+            String s2 = secondPart.get(i);
+            if (!s1.equals(s2)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
---
+++ src/test/java/adventOfCode/Year2023/Day13Test.java
@@ -0,0 +1,109 @@
+package adventOfCode.Year2023;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day13Test {
+
+    @Test
+    void verticalReflection() {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("#...##..#");
+        arr.add("#....#..#");
+        arr.add("..##..###");
+        arr.add("#####.##.");
+        arr.add("#####.##.");
+        arr.add("..##..###");
+        arr.add("#....#..#");
+        assertEquals(4, Day13.horizontalReflection(arr, false));
+    }
+
+    @Test
+    void horizontalReflection1() {
+        ArrayList<String> arr = new ArrayList<>();
+        String s = "..#...###..##\n" +
+                "..##....##...\n" +
+                "#.#.#....####\n" +
+                "##.###.#.##.#\n" +
+                "##.###.#.##.#\n" +
+                "#.#.#....####\n" +
+                ".###....##...\n" +
+                "..#...###..##\n" +
+                "..#...###..##\n" +
+                ".###....##...\n" +
+                "#.#.#....####";
+        StringBuilder s1= new StringBuilder();
+        for (char c : s.toCharArray()) {
+
+            if (c =='\n') {
+
+                arr.add(s1.toString());
+                s1=new StringBuilder();
+            }else{
+
+            s1.append(c);
+            }
+        }
+        assertEquals(4, Day13.horizontalReflection(arr, false));
+    }
+
+    @Test
+    void horizontalReflection() {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("#...##..#");
+        arr.add("#....#..#");
+        arr.add("..##..###");
+        arr.add("#####.##.");
+        arr.add("#####.##.");
+        arr.add("..##..###");
+        arr.add("#....#..#");
+        assertEquals(8, Day13.horizontalReflection(arr, false));
+    }
+
+    @Test
+    void transposeArray() {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("#...##..#");
+        arr.add("#....#..#");
+        arr.add("..##..###");
+        arr.add("#####.##.");
+        arr.add("#####.##.");
+        arr.add("..##..###");
+        arr.add("#....#..#");
+        ArrayList<String> transpedArr = Day13.transposeArray(arr);
+        for (String s : arr) {
+            System.out.println(s);
+        }
+        System.out.println();
+        for (String s : transpedArr) {
+            System.out.println(s);
+        }
+    }
+
+    @Test
+    void transposeArray2() {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("123");
+        arr.add("456");
+        arr.add("789");
+
+        ArrayList<String> expectedArr = new ArrayList<>();
+        expectedArr.add("369");
+        expectedArr.add("258");
+        expectedArr.add("147");
+
+
+        ArrayList<String> transpedArr = Day13.transposeArray(arr);
+        for (String s : expectedArr) {
+            System.out.println(s);
+        }
+        System.out.println();
+        for (String s : transpedArr) {
+            System.out.println(s);
+        }
+        assertEquals(expectedArr, transpedArr);
+    }
+}
