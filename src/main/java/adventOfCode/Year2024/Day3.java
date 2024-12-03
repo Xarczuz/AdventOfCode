1da818847592421f1542a453398843ed69caed48
Xarczuz <Xarczuz 44897681+xarczuz@users.noreply.github.com>
2024-12-03 08:47:48 +0100

    2024 Day 3 part 2 DONE

--- src/main/java/adventOfCode/Year2024/Day3.java
+++ src/main/java/adventOfCode/Year2024/Day3.java
@@ -9,17 +9,17 @@ import util.TimeUtil;
 public class Day3 {
 
   public static void main(String[] args) throws IOException, URISyntaxException {
-
     System.out.println("(11,8)".matches("\\(\\d+,\\d+\\).{0,}"));
     List<String> l = FileUtil.readfile(Day3.class);
     List<String> l2 = FileUtil.readfileExempel(Day3.class);
+    List<String> l3 = FileUtil.readfileExempel2(Day3.class);
     TimeUtil.startTime();
     oneStar(l);
     oneStar(l2);
     TimeUtil.endTime();
     TimeUtil.startTime();
     twoStar(l);
-    twoStar(l2);
+    twoStar(l3);
     TimeUtil.endTime();
   }
 
@@ -65,11 +65,6 @@ public class Day3 {
     for (String s : l) {
       String[] muls = s.split("mul");
       for (String mul : muls) {
-        if (mul.contains("do()")) {
-          active = true;
-        } else if (mul.contains("don't()")) {
-          active = false;
-        }
         String[] split = mul.split(",");
         if (split.length > 1) {
           String s1 = split[0];
@@ -82,7 +77,13 @@ public class Day3 {
               int i1 = Integer.parseInt(substring2);
               sum += (long) i * i1;
             }
+
           }
+        }
+        if (mul.contains("do()")) {
+          active = true;
+        } else if (mul.contains("don't()")) {
+          active = false;
         }
       }
     }
